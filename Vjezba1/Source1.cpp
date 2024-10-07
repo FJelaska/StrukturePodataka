31d24687b2367c909aa6b7944d7403e0ad6be061
FJelaska <frane.jelaska@gmail.com>
2024-10-07 19:49:21 +0200

    pola1

--- Vjezba1/Source1.cpp
+++ Vjezba1/Source1.cpp
@@ -1,6 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS_
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX 1024
+
+
+int brRed(const char*);
 
 int main() {
-	printf("Hello world!");		
+
+	int brred = 0;
+
+	brred = brRed("imedat.txt");
+
+	return 0;
+}
+
+
+int brRed(const char*fileName) {
+
+	FILE*fp = NULL;
+	int brRed = 0;
+	char buffer[MAX] = {};
+
+	fp = fopen(fileName, "r");
+	if (!fp)
+		return -1;
+
+	while (!feof(fp)) {
+		fgets(buffer, MAX, fp);
+		brRed++;
+	}
+
+	fclose(fp);
 	return 0;
 }

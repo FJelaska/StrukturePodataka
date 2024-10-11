d8b1a2811f19673d2f8d0a95a34cb77a7231c2fd
FJelaska <frane.jelaska@gmail.com>
2024-10-11 21:57:17 +0200

    1 zad u cjelosti

--- Vjezba1/Source1.cpp
+++ Vjezba1/Source1.cpp
@@ -1,36 +1,56 @@
-#define _CRT_SECURE_NO_WARNINGS_
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX 1024
 
+typedef struct {
+	char ime[50];
+	char prezime[50];
+	double bodovi;
+}Student;
 
-int brRed(const char*);
+int upis(FILE *fp, Student *s, int n);
 
 int main() {
 
-	int brred = 0;
+	int br = 0;
+	FILE* fp = NULL;
 
-	brred = brRed("imedat.txt");
+	fp = fopen("studenti1.txt", "r");
+	if (fp == NULL) {
+		printf("Greska 1");
+		return 1;
+	}
+
+	while (!feof(fp)) {
+		if (fgetc(fp) == '\n')
+			br++;
+	}
+
+	Student* s = NULL;
+	s = (Student*)malloc(br * sizeof(Student));
+	if (s == NULL)
+		printf("Greska 2");
+
+	rewind(fp);
+	upis(fp, s, br);
+	fclose(fp);
 
 	return 0;
 }
 
+int upis(FILE* fp, Student* s, int n) {
 
-int brRed(const char*fileName) {
+	int i, max = 50;
+	double rel;
 
-	FILE*fp = NULL;
-	int brRed = 0;
-	char buffer[MAX] = {};
-
-	fp = fopen(fileName, "r");
-	if (!fp)
-		return -1;
-
-	while (!feof(fp)) {
-		fgets(buffer, MAX, fp);
-		brRed++;
+	for (i = 0; i < n; i++) {
+		if (fscanf(fp, "%s %s %lf\n", (*(s + i)).ime, (*(s + i)).prezime, &(*(s + i)).bodovi) == 3) {
+			rel = s[i].bodovi / max * 100;
+			printf("Ime i prezime: %s %s\t Bodovi: %.2lf\t Rel. bodovi: %.2lf\n", (*(s + i)).ime, (*(s + i)).prezime, (*(s + i)).bodovi, rel);
+		}
+		else
+			printf("Greska 3");
 	}
-
-	fclose(fp);
 	return 0;
 }
